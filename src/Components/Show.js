05c80dd6d35156ed5232b52ceb038b3addfa4ba0
jcoops22 <jcooper232074@gmail.com>
2020-05-29 20:08:48 -0600

    still messing with loading animations

--- src/Components/Show.js
+++ src/Components/Show.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import { device } from "../resources/mediaquery";
 import { Link } from "react-router-dom";
+import Typer from "./Typer";
 import galaxy from "../resources/Icons/galaxy.svg";
 import galaxy2 from "../resources/Icons/galaxy2.svg";
 import galaxylight from "../resources/Icons/galaxylight.svg";
@@ -12,23 +13,39 @@ const Show = (props) => {
   const project = props.location.state.project;
   const loader = () => {
     let player = document.querySelector(".react_player");
-    console.log("we ready");
-    player.style.opacity = 1;
-    setPlaying(true);
+    let load = document.querySelectorAll(".load");
+    setTimeout(() => {
+      load.forEach((loader) => {
+        loader.style.animationDuration = "20s";
+      });
+    }, 1000);
+    setTimeout(() => {
+      load.forEach((loader) => {
+        loader.style.display = "none";
+      });
+      player.style.opacity = 1;
+      setPlaying(true);
+    }, 3000);
   };
-  // <Img width="10%" src={galaxy} name={"spin"} />
-  //         <Img width="10%" src={galaxylight} name={"cycle"} />
+
   return (
-    <div>
+    <Container>
       <Link to="/projects">
         <Button>Back</Button>
       </Link>
       <H1>{project.name}</H1>
       <Desc>{project.desc}</Desc>
       <Vid>
-        <Loading>
-          Loading
-          <Img width="10%" src={galaxy2} name={"cycle"} />
+        <Loading className="loadingWrapper">
+          <Typer
+            sentence="Loading..."
+            underline="false"
+            icon="false"
+            fontSize={"2%"}
+          />
+          <Img width="8%" className="load" src={galaxy} name={"spin"} />
+          <Img width="8%" className="load" src={galaxylight} name={"cycle"} />
+          <Img width="8%" className="load" src={galaxy2} name={"cycle2"} />
         </Loading>
         <ReactPlayer
           className="react_player"
@@ -48,13 +65,19 @@ const Show = (props) => {
           <Button>Visit Site</Button>
         </Link>
       </div>
-    </div>
+    </Container>
   );
 };
 
 export default Show;
 const H1 = styled.h1`
   color: white;
+`;
+const Container = styled.div`
+  height: 90vh;
+  width: 100%;
+  overflow: scroll;
+  border: 1px solid red;
 `;
 const Button = styled.button`
   cursor: pointer;
@@ -66,13 +89,14 @@ const Button = styled.button`
 const Img = styled.img`
   position: absolute;
   top: 0;
-  left: 0;
+  left: 45%;
+  margin-top: 25%;
   animation-name: ${(props) => props.name};
-  animation-duration: 4s;
+  animation-duration: 2s;
   animation-iteration-count: infinite;
   animation-timing-function: linear;
   animation-direction: forwards;
-  border: 1px solid blue;
+  /* border: 1px solid blue; */
 `;
 const Desc = styled.div`
   color: #fff;
@@ -82,6 +106,7 @@ const Vid = styled.div`
   border: 1px solid greenyellow;
   position: relative;
   padding: 56.25% 2rem 0;
+  width: 100%;
 `;
 const Loading = styled.div`
   position: absolute;
@@ -93,6 +118,6 @@ const Loading = styled.div`
   align-items: center;
   width: 100%;
   height: 100%;
-  border: 1px solid red;
+  /* border: 1px solid red; */
   color: #fff;
 `;
